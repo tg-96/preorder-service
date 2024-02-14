d627926589e3505b6cdad91697f42302cfb6b3f6
Jeong tae gyu <jtg960528@gmail.com>
2024-02-14 15:31:42 +0900

    test: 상품 상세페이지 테스트

--- preorderService/itemService/src/main/java/com/preOrderService/entity/Item.java
+++ preorderService/itemService/src/main/java/com/preOrderService/entity/Item.java
@@ -6,8 +6,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Entity
--- preorderService/itemService/src/test/java/com/preOrderService/service/ItemServiceTest.java
+++ preorderService/itemService/src/test/java/com/preOrderService/service/ItemServiceTest.java
@@ -5,6 +5,7 @@ import com.preOrderService.entity.Item;
 import com.preOrderService.exception.ItemServiceException;
 import com.preOrderService.repository.ItemRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,8 +15,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
@@ -27,21 +29,23 @@ class ItemServiceTest {
 
     @InjectMocks
     private ItemService itemService;
+
     @Test
+    @DisplayName("상품 리스트 조회 테스트")
     void getAllItems() {
         //given
         when(itemRepository.findAll()).thenReturn(
                 Arrays.asList(Item.generalItemCreate(
-                        "세탁기",
-                        "드럼 세탁기 입니다.",
-                        10000,
-                        10
-                        ),Item.reserveItemCreate(
+                                "세탁기",
+                                "드럼 세탁기 입니다.",
+                                10000,
+                                10
+                        ), Item.reserveItemCreate(
                                 "냉장고",
-                        "좋은 냉장고",
-                        50000,
-                        3,
-                        LocalDateTime.of(2024,2,15,10,00))
+                                "좋은 냉장고",
+                                50000,
+                                3,
+                                LocalDateTime.of(2024, 2, 15, 10, 00))
                 )
         );
         //when
@@ -53,8 +57,34 @@ class ItemServiceTest {
 
         //exception
         when(itemRepository.findAll()).thenReturn(Arrays.asList());
-        assertThrows(ItemServiceException.class,() -> {
-         itemService.getAllItems();
+        assertThrows(ItemServiceException.class, () -> {
+            itemService.getAllItems();
+        });
+    }
+
+    @Test
+    @DisplayName("상품 정보 조회")
+    void getItemInfo() {
+        //given
+        when(itemRepository.findById(1L)).thenReturn(Optional.of(
+                        Item.generalItemCreate(
+                                "세탁기",
+                                "좋은 세탁기 입니다.",
+                                1000,
+                                10
+                        )
+                )
+        );
+        //when
+        ItemResponseDto itemInfo = itemService.getItemInfo(1L);
+
+        //then
+        assertThat(itemInfo.getName()).isEqualTo("세탁기");
+
+        //exception
+        when(itemRepository.findById(1L)).thenReturn(Optional.empty());
+        Assertions.assertThrows(ItemServiceException.class, () -> {
+            itemService.getItemInfo(1L);
         });
     }
 }
